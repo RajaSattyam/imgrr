7b4ef75a7982ac559d6e58181fcb87ed1480b397
Abinav Seelan <abinav.n.seelan@gmail.com>
2017-08-19 23:39:39 +0530

    Add image upload validation

--- web-server/client/containers/Home/index.js
+++ web-server/client/containers/Home/index.js
@@ -1,4 +1,4 @@
-/* global document */
+/* global window Image */
 import React, { Component } from 'react';
 import request from 'axios';
 
@@ -7,17 +7,47 @@ import styles from './index.scss';
 class Home extends Component {
   constructor(args) {
     super(args);
+    this.state = {
+      uploadImage: null,
+      uploadImageId: null,
+      error: '',
+      images: [],
+    };
     this.handleUpload = this.handleUpload.bind(this);
+    this.validateUploadImage = this.validateUploadImage.bind(this);
   }
 
   componentWillMount() {
 
   }
 
+  validateUploadImage(event) {
+    const files = event.target.files;
+    const file = files && files[0];
+    if (file.type === 'image/png') {
+      const img = new Image();
+      const self = this;
+      img.src = window.URL.createObjectURL(file);
+      img.onload = function loadImage() {
+        if (this.width === 1024 || this.height === 1024) {
+          // Upload Image to Image Service
+        } else {
+          self.setState({
+            error: 'Image dimensions should be 1024 x 1024',
+          });
+        }
+      };
+    } else {
+      this.setState({
+        error: 'Please upload an image!',
+      });
+    }
+  }
+
   handleUpload(event) {
     event.preventDefault();
     const data = {
-      id: 'test_id',
+      id: this.state.uploadImageId,
     };
 
     request
@@ -30,11 +60,14 @@ class Home extends Component {
     return (
       <div className={styles.container}>
         <div className={styles.uploader}>
+          <div>
+            {this.state.error}
+          </div>
           <input
             type="file"
             placholder="Upload an image"
             className={styles.input}
-            id="fileUpload"
+            onChange={this.validateUploadImage}
           />
           <button className={styles['upload-btn']} onClick={this.handleUpload}>Upload</button>
         </div>
